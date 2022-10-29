eeba1162df796d539845a6263e1585c4861e6a21
joao.lima08 <joao.lima@bagy.com.br>
2022-10-29 15:50:20 -0300

    Feat: Add stopwatch

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -14,15 +14,15 @@
                 <div
                     class="is-flex is-align-items-center is-justify-content-space-between">
                     <section>
-                        <strong>00:00:00</strong>
+                        <strong>{{ tempoDecorrido }}</strong>
                     </section>
-                    <button class="button">
+                    <button class="button" @click="iniciar">
                         <span class="icon">
                             <i class="fas fa-play"></i>
                         </span>
                         <span>Play</span>
                     </button>
-                    <button class="button">
+                    <button class="button" @click="parar">
                         <span class="icon">
                             <i class="fas fa-stop"></i>
                         </span>
@@ -38,5 +38,28 @@
 import { defineComponent } from 'vue';
 export default defineComponent({
     name: 'FormularioComponent',
+    data() {
+        return {
+            tempoEmSegundos: 0,
+            cronometro: 0,
+        };
+    },
+    computed: {
+        tempoDecorrido(): string {
+            return new Date(this.tempoEmSegundos * 1000)
+                .toISOString()
+                .substring(11, 19);
+        },
+    },
+    methods: {
+        iniciar() {
+            this.cronometro = setInterval(() => {
+                this.tempoEmSegundos += 1;
+            }, 1000);
+        },
+        parar() {
+            clearInterval(this.cronometro);
+        },
+    },
 });
 </script>
