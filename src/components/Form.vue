3a2a562804f633b14d889096a2faed5f7338df3b
joao.lima08 <joao.lima@bagy.com.br>
2022-11-08 20:40:11 -0300

    Feat: Create Timer component and disable logic to start and stop button

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -11,22 +11,7 @@
                     placeholder="Qual tarefa deseja iniciar?" />
             </div>
             <div class="column">
-                <div
-                    class="is-flex is-align-items-center is-justify-content-space-between">
-                    <StopWatch :seconds="seconds" />
-                    <button class="button" @click="start">
-                        <span class="icon">
-                            <i class="fas fa-play"></i>
-                        </span>
-                        <span>Play</span>
-                    </button>
-                    <button class="button" @click="stop">
-                        <span class="icon">
-                            <i class="fas fa-stop"></i>
-                        </span>
-                        <span>Stop</span>
-                    </button>
-                </div>
+                <Timer />
             </div>
         </div>
     </div>
@@ -34,28 +19,12 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import StopWatch from './StopWatch.vue';
+import Timer from './Timer.vue';
 
 export default defineComponent({
     name: 'FormComponent',
     components: {
-        StopWatch,
-    },
-    data() {
-        return {
-            seconds: 0,
-            stopWatch: 0,
-        };
-    },
-    methods: {
-        start() {
-            this.stopWatch = setInterval(() => {
-                this.seconds += 1;
-            }, 1000);
-        },
-        stop() {
-            clearInterval(this.stopWatch);
-        },
+        Timer,
     },
 });
 </script>
---
+++ src/components/Timer.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="is-flex is-align-items-center is-justify-content-space-between">
+        <StopWatch :seconds="seconds" />
+        <button class="button" @click="start" :disabled="isStarted">
+            <span class="icon">
+                <i class="fas fa-play"></i>
+            </span>
+            <span>Play</span>
+        </button>
+        <button class="button" @click="stop" :disabled="!isStarted">
+            <span class="icon">
+                <i class="fas fa-stop"></i>
+            </span>
+            <span>Stop</span>
+        </button>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import StopWatch from './StopWatch.vue';
+
+export default defineComponent({
+    name: 'TimerComponent',
+    components: {
+        StopWatch,
+    },
+    data() {
+        return {
+            seconds: 0,
+            interval: 0,
+            isStarted: false,
+        };
+    },
+    methods: {
+        start() {
+            this.isStarted = true;
+            this.interval = setInterval(() => {
+                this.seconds += 1;
+            }, 1000);
+        },
+        stop() {
+            this.isStarted = false;
+            clearInterval(this.interval);
+        },
+    },
+});
+</script>
